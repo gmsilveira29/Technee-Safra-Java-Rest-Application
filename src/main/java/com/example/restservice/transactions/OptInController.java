f8621954e81efd002e3960d3725b98054845a7e3
Gustavo Silveira <gustavo.silveira@summit-bra.com>
2020-10-07 00:08:00 -0300

    remove sensitive data

--- src/main/java/com/example/restservice/transactions/OptInController.java
+++ src/main/java/com/example/restservice/transactions/OptInController.java
@@ -18,7 +18,7 @@ public class OptInController {
         ResponseEntity<String> response = null;
         RestTemplate restTemplate = new RestTemplate();
         //url
-        String url = "https://af3tqle6wgdocsdirzlfrq7w5m.apigateway.sa-saopaulo-1.oci.customer-oci.com/fiap-sandbox/accounts/v1/optin";
+        String url = "...";
 
         //header
         HttpHeaders headers = new HttpHeaders();
